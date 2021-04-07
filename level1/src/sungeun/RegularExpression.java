8bb5d12adf13e5e0eb1b6980095f9f358facd924
goldenpuu <mizunzxc@gmail.com>
2021-04-07 12:31:47 +0900

    add Regular Expression practice

---
+++ level1/src/sungeun/RegularExpression.java
@@ -0,0 +1,39 @@
+package sungeun;
+
+class RegularExpression {
+    public String solution(String new_id) {
+        String answer = "";
+        
+        //1단계
+        answer = new_id.toLowerCase();
+        //2단계
+        answer = answer.replaceAll("[^a-z|0-9|_|.|-]", "");
+        //3단계
+        answer = answer.replaceAll("\\.{2,}", ".");
+        //4단계
+        if(answer.startsWith(".")){
+            answer = answer.replaceFirst(".","");
+        }
+        if(answer.endsWith(".")){
+            answer = answer.replaceAll(".$","");
+        }
+        //5단계
+        if("".equals(answer)){
+            answer="a";
+        }
+        //6단계
+        if(answer.length() > 15){
+            answer = answer.substring(0,15);
+            
+            if(answer.endsWith(".")){
+                answer = answer.replaceAll(".$","");
+            }
+        }
+        //7단계
+        while(answer.length() < 3){
+            answer += answer.charAt(answer.length()-1);
+        }
+        
+        return answer;
+    }
+}
