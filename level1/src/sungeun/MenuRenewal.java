2f8a55f1f564c57ce1d6b25aae25b3ed54a86218
goldenpuu <mizunzxc@gmail.com>
2021-04-21 22:49:02 +0900

    issue : 2중 for문을 사용하여 리스트의 모든 경우의 수를 검사하는 데 한계가 있음, 재귀함수를 사용하여 다시 작성해볼 예정/ We are faced to some limit about checking the list. So We are going to use recursion function.

--- level1/src/sungeun/MenuRenewal.java
+++ level1/src/sungeun/MenuRenewal.java
@@ -4,11 +4,15 @@ import java.util.Iterator;
 import java.util.Set;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.List;
+import java.util.Arrays;
+import java.util.ArrayList;
 
 public class MenuRenewal {
 	class Solution {
 	    public String[] solution(String[] orders, int[] course) {
-	        String[] answer = {};
+	        String[] answer ={};
+	        List<String> answerList = new ArrayList<String>();
 	        
 	        int cl; // Course Length
 	        String ol; // Order list
@@ -18,11 +22,13 @@ public class MenuRenewal {
 	        for(int i = 0; i < course.length; i++){
 	            ol = "";
 	            cl = course[i];
+	            set.clear();
+	            strMap.clear();
 	            
 	            // add unique Characters to set 
 	            for(String str: orders){
 	                for(int n = 0; n < str.length(); n++){
-	                    set.add(str.charAt(n)); 
+	                    set.add(str.charAt(n));
 	                }
 	            }
 	            
@@ -31,22 +37,61 @@ public class MenuRenewal {
 	            while(it.hasNext()){
 	                ol += it.next();
 	            }
-	            System.out.println(ol);
+	            // System.out.println(ol);
 	            
-	            for(int x = 1,b = 1, f = cl-b; x < ol.length()-b; x++,b++,f=cl-b){
-	                System.out.println("f = "+f);
-	                System.out.println("b = "+b);
-	                for(int y = x+1; y < ol.length(); y++){
-	                    
+	            int b_idx,b_len,r_idx,r_len=1;
+	            String str = "";
+	            int front_box_size = 1, after_box_size;
+	            int max_num = 0;
+	            
+	            while(front_box_size < cl){
+	                after_box_size = cl-front_box_size;
+	                for(String cor_ord : orders){
+	                    for(b_idx=0, b_len=front_box_size; (b_idx+b_len+r_len) <= cor_ord.length(); b_idx++){
+	                        for(r_idx=(b_len==1) ? b_idx+b_len : b_idx+b_len+1, r_len=after_box_size; (r_idx+r_len) <= cor_ord.length(); r_idx++){
+	                            for(int j = 0; j<b_len; j++){
+	                                str += Character.toString(cor_ord.charAt(j+b_idx));
+	                            }
+	                            for(int j = 0; j<r_len; j++){
+	                                str += Character.toString(cor_ord.charAt(j+r_idx));
+	                            }
+	                            char[] asd = str.toCharArray();
+	                            Arrays.sort(asd);
+	                            str = new String(asd);
+	                            
+	                            if(strMap.containsKey(str)){
+	                                strMap.put(str,strMap.get(str)+1);
+	                            }else{
+	                                strMap.put(str,0);
+	                            }
+	                            str="";
+	                        }
+	                    }
+	                }
+	                front_box_size++;
+	            }
+	            // System.out.println(strMap.toString());
+	            for(int num : strMap.values()){
+	                if(num > max_num){
+	                    max_num = num;
 	                }
 	            }
-	            
-	            
-	            
+	            if(max_num > 0){
+	                for(Map.Entry<String, Integer> entry : strMap.entrySet()){
+	                    if(entry.getValue() == max_num){
+	                        answerList.add(entry.getKey());
+	                    }
+	                }
+	            }
+	        }
+	        answer = new String[answerList.size()];
+	        int i = 0;
+	        for(String s : answerList){
+	            answer[i] = s;
+	            i++;
 	        }
 	        
-	        
+	        Arrays.sort(answer);
 	        return answer;
 	    }
-	}
 }
