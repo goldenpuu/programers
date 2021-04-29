13a1ed27fe3611aa70eec67e28c88a7e8a741ee7
goldenpuu <mizunzxc@gmail.com>
2021-04-29 19:09:10 +0900

    retry

--- SungeunAlgorithmStudyPlace/src/devcat/Q1_StreamFiltering.java
+++ SungeunAlgorithmStudyPlace/src/devcat/Q1_StreamFiltering.java
@@ -3,7 +3,7 @@ package devcat;
 import java.util.Arrays;
 import java.util.stream.LongStream;
 
-public class OneLongStream {
+public class Q1_StreamFiltering {
 	public int[] solution(int[] lottos, int[]winNums) {
 		return LongStream.of(
 				(lottos.length + 1) - Arrays.stream(lottos).filter(l -> Arrays.stream(winNums).anyMatch(w -> w == l) || l == 0).count(),
@@ -12,4 +12,7 @@ public class OneLongStream {
 	}
 }
 //1. LongStream 클래스에서 of(value, vlaue2, ...)메서드 실행 / 인자가 복수일 경우 stream 반환
-//2. value = 7 - 
+//2. value = 7 - (맞은 복권의 갯수 + 0의 갯수)
+//3. value2 = 7 - (맞은 복권의 갯수)
+//4. mapToInt메서드로 7이상의 값에 빼기 1을 처리한 stream 반환
+//5. 을 int배열로 반환
